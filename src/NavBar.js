1b73ded165797880baf4d94704e8ff98888e7583
Saugat <pdl.saugat07@gmail.com>
2025-09-18 14:47:38 +0545

    Splitting components

---
+++ src/NavBar.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+export default function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <NumResults />
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>Movies log</h1>
+    </div>
+  );
+}
+function Search() {
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+function NumResults() {
+  return (
+    <p className="num-results">
+      Found <strong>x</strong> results
+    </p>
+  );
+}
